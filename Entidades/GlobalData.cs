9f0ad8298a7d6eb782f2f86abd252855473e10ae
Jose Manuel Echeverri Palacio <jose.echeverri@upb.edu.co>
2018-11-17 20:28:31 -0500

    Carrito version 1

---
+++ Entidades/GlobalData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class GlobalData
+    {
+        public static List<int> car = new List<int>();
+
+        public static void annadirCarrito(int id) {
+
+            car.Add(id);
+           
+        }
+        
+        public static List<int> getCar()
+        {
+
+            return car;
+
+        }
+
+
+    };
+}
---
+++ SitioTienda/App_Code/CarritoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EN = Entidades;
+using CT = Controladora;
+
+public class CarritoController : ApiController
+{
+    CT.Productos ct = new CT.Productos();
+
+    // GET api/<controller>
+    public List<EN.Producto> Get()
+    {
+        List<int> productos = new List<int>();
+        List<EN.Producto> producto = new List<EN.Producto>();
+
+        foreach (int item in productos)
+        {
+
+            producto.Add(ct.GetProducto(item,true));
+
+        }
+        return producto;
+    }
+
+    // GET api/<controller>/5
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // POST api/<controller>
+    public void Post(int id)
+    {
+        EN.GlobalData.annadirCarrito(id);
+    }
+
+    // PUT api/<controller>/5
+    public void Put(int id, [FromBody]string value)
+    {
+    }
+
+    // DELETE api/<controller>/5
+    public void Delete(int id)
+    {
+    }
+}
